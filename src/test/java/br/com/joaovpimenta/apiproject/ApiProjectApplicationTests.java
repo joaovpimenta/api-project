a3deb6437e29500acd4c99c31e8834a96df90014
joaovpimenta <joaovpiment@gmail.com>
2022-11-11 23:16:07 -0300

    Initial Commit

---
+++ src/test/java/br/com/joaovpimenta/apiproject/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaovpimenta.apiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
